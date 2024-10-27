5ad6065af5cd191b5d06c745d2ca5046b379a236
Azael Duarte <azaelduarte999@gmail.com>
2024-10-26 23:54:58 -0600

    ajustes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'about',
+        loadComponent: () => import('../app/pages/about-pages/about-pages.component')
+    },
+    {
+        path: 'pricing',
+        loadComponent: () => import('../app/pages/pricing-page/pricing-page.component')
+    },
+    {
+        path: 'contact',
+        loadComponent: () => import('../app/pages/contact/contact.component')
+    },
+    {
+        path: '**',
+        redirectTo: () => {
+            return 'about'
+        }
+    }
+];
